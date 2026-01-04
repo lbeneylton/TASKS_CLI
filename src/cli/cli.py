573abc2553e104495dcbc8c0103cef7dd13bfd69
Lucas Beneylton <47756391+lbeneylton@users.noreply.github.com>
2026-01-04 20:10:09 -0300

    testes para cli

--- src/cli/cli.py
+++ src/cli/cli.py
@@ -2,4 +2,17 @@ import argparse
 
 
 def main():
-    print("pacote okay")
+    parser = argparse.ArgumentParser(
+        prog="task",
+        description="Uma ferramenta de linha de comando para gerenciar tarefas.",
+        epilog="Use 'task --help' para ver opções específicas."
+    )
+
+    parser.add_argument(
+        "add",
+        type=str,
+        help="Adiciona uma nova tarefa."
+    )
+
+    args = parser.parse_args()
+    print(args)
